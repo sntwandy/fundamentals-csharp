b758facfae839bf7b14399af4d8f754ca73b68c1
sntwandy <sntwandy@gmail.com>
2021-07-21 08:06:04 -0400

    Understanding objects and class

--- Program.cs
+++ Program.cs
@@ -2,11 +2,32 @@
 
 namespace net
 {
+  class School {
+    /* Attributes */
+    public string name;
+    public string address;
+    public int yearFoundation;
+
+    internal string ceo = "Megan Herrera";
+
+    public void Sound ()
+    {
+      /* TODO */
+      Console.Beep(5000, 3000);
+    }
+  }
   class Program
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      // Console.WriteLine("Hello World!");
+      var mySchool = new School();
+
+      mySchool.name = "Platzi Academy";
+      mySchool.address = "Calle de la Plata, 1";
+      mySchool.yearFoundation = 1999;
+      Console.WriteLine("SOUND");
+      mySchool.Sound();
     }
   }
 }
